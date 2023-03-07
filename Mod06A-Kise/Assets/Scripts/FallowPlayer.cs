3a72476981069828dc91a55f70b59235222a689d
sherekhan9 <Sherekhan9@gmail.com>
2023-03-07 13:31:04 -0600

    Mod06A-Kise

--- Mod06A-Kise/Assets/Scripts/FallowPlayer.cs
+++ Mod06A-Kise/Assets/Scripts/FallowPlayer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FallowPlayer : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
-    public float speed = 5.0f;
     void Start()
     {
         
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + new Vector3(0,5,-7);
     }
 }
